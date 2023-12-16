de397f130e1fd69e79333d6eb409aca546c8bfb8
alirezarastineh <kouroshrastineh95@gmail.com>
2023-12-16 15:43:59 +0100

    create user router

---
+++ server/routes/user.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { registrationUser } from "../controllers/user.controller";
+
+const userRouter = express.Router();
+
+userRouter.post("/registration", registrationUser);
+
+export default userRouter;
