873eefa542cd20511f5791ef728e9d363daed04c
alirezarastineh <kouroshrastineh95@gmail.com>
2023-12-16 15:43:44 +0100

    add user router

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,14 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleWare } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 require("dotenv").config();
 
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
+
 app.use(cookieParser());
 
 app.use(
@@ -15,6 +17,8 @@ app.use(
     origin: process.env.ORIGIN,
   })
 );
+
+app.use("/api/v1", userRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
