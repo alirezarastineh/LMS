e3d9035e0401e69edebb7a3c66b6756641ee5291
alirezarastineh <kouroshrastineh95@gmail.com>
2023-12-28 14:18:34 +0100

    deleted comments

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -26,13 +26,10 @@ const sendMail = async (options: EmailOptions): Promise<void> => {
 
   const { email, subject, template, data } = options;
 
-  // get the path to the email template file
   const templatePath = path.join(__dirname, "../mails", template);
 
-  // Render the email template with EJS
   const html: string = await ejs.renderFile(templatePath, data);
 
-  // Send the email
   const mailOptions = {
     from: process.env.SMTP_MAIL,
     to: email,
