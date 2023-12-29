ff0ca71ba272da2248004d4371b4a0a09c5b6f2b
alirezarastineh <kouroshrastineh95@gmail.com>
2023-12-29 15:15:03 +0100

    creating global declaration

---
+++ server/@types/custom.d.ts
@@ -0,0 +1,10 @@
+import { IUser } from "./../models/user.model";
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user? = IUser;
+    }
+  }
+}
