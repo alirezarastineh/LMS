8ec4b1f0d569f62756c34633f4e3e9bc7d420358
alirezarastineh <kouroshrastineh95@gmail.com>
2023-12-31 15:11:41 +0100

    adding routs for updating access token, getting user information and social authorization

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   activateUser,
+  getUserInfo,
   loginUser,
   logoutUser,
   registrationUser,
+  socialAuth,
+  updateAccessToken,
 } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
@@ -16,5 +19,11 @@ userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
 
 userRouter.get("/logout", isAuthenticated, logoutUser);
+
+userRouter.get("/refresh", updateAccessToken);
+
+userRouter.get("/me", isAuthenticated, getUserInfo);
+
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
