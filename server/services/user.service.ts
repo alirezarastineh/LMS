d300d73c68fc9e936015de0f7e0cb6537092d158
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-02 14:21:08 +0100

    add redis to get user by id

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,10 +1,14 @@
-import { NextFunction, Request, Response } from "express";
+import { Response } from "express";
 import userModel from "../models/user.model";
+import { redis } from "../utils/redis";
 
 export const getUserById = async (id: string, res: Response) => {
-  const user = await userModel.findById(id);
-  res.status(201).json({
-    success: true,
-    user,
-  });
+  const userJson = await redis.get(id);
+  if (userJson) {
+    const user = await userModel.findById(id);
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  }
 };
