99d5a6794ffe2a5ad213b36eb884df3cfd2c9fc1
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-03 11:24:57 +0100

    correct the userJson logic

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,11 +1,10 @@
 import { Response } from "express";
-import userModel from "../models/user.model";
 import { redis } from "../utils/redis";
 
 export const getUserById = async (id: string, res: Response) => {
   const userJson = await redis.get(id);
   if (userJson) {
-    const user = await userModel.findById(id);
+    const user = JSON.parse(userJson);
     res.status(201).json({
       success: true,
       user,
