ee6b433cd113e5af1345e28bf0b29661f9804b80
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-03 14:25:13 +0100

    delete unnecessary comment

--- server/services/course.service.ts
+++ server/services/course.service.ts
@@ -2,7 +2,6 @@ import { Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import CourseModel from "../models/course.model";
 
-// create course
 export const createCourse = CatchAsyncError(
   async (data: any, res: Response) => {
     const course = await CourseModel.create(data);
