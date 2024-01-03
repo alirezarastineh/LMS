dba365b889677eafa8b31af0df4197c05812fc2a
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-03 14:24:38 +0100

    edit course rout added

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
@@ -9,6 +9,13 @@ courseRouter.post(
   isAuthenticated,
   authorizeRoles("admin"),
   uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse
 );
 
 export default courseRouter;
