36f5b3780b4692c231ddf6c1df0c9da8bd57863b
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-05 12:52:19 +0100

    adding cron logic to delete read notifications after 30 days

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import NotificationModel from "../models/notification.model";
+import cron from "node-cron";
 
 export const getAllNotifications = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -47,3 +48,11 @@ export const updateNotificationStatus = CatchAsyncError(
     }
   }
 );
+
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
