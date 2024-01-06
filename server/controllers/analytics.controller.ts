1491d9ed50e51c9c63cf40fdad56212356e8e166
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-06 12:57:59 +0100

    creating analytics controller - adding get the analytics for all users

---
+++ server/controllers/analytics.controller.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import { generateLast12MothsData } from "../utils/analytics.generator";
+import UserModel from "../models/user.model";
+
+export const getUsersAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await generateLast12MothsData(UserModel);
+
+      res.status(200).json({
+        success: true,
+        users,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
