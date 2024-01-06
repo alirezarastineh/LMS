b1b3c9b4740b6d3088fcc6d0b4e9d9d7ff32c0ec
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-06 15:55:25 +0100

    edit layout for admin and get layout logics added

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -78,3 +78,107 @@ export const createLayout = CatchAsyncError(
     }
   }
 );
+
+export const editLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+
+      if (type === "FAQ") {
+        const { faq } = req.body;
+
+        const faqData = await LayoutModel.findOne({ type: "FAQ" });
+
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+
+        await LayoutModel.findByIdAndUpdate(faqData?._id, {
+          type: "FAQ",
+          faq: faqItems,
+        });
+      }
+
+      if (type === "Categories") {
+        const { categories } = req.body;
+
+        const categoriesData = await LayoutModel.findOne({
+          type: "Categories",
+        });
+
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+
+        await LayoutModel.findByIdAndUpdate(categoriesData?._id, {
+          type: "Categories",
+          categories: categoriesItems,
+        });
+      }
+
+      if (type === "Banner") {
+        const bannerData: any = await LayoutModel.findOne({ type: "Banner" });
+
+        const { image, title, subtitle } = req.body;
+
+        const data = image.startsWith("https")
+          ? bannerData
+          : await cloudinary.v2.uploader.upload(image, {
+              folder: "Layout",
+            });
+
+        const banner = {
+          type: "Banner",
+          banner: {
+            image: {
+              public_id: image.startsWith("https")
+                ? bannerData.banner.image.public_id
+                : data?.public_id,
+              url: image.startsWith("https")
+                ? bannerData.banner.image.url
+                : data?.secure_url,
+            },
+            title,
+            subtitle,
+          },
+        };
+
+        await LayoutModel.findByIdAndUpdate(bannerData._id, { banner });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Layout updated successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// get layout by type
+export const getLayoutByType = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.params;
+
+      const layout = await LayoutModel.findOne({ type });
+
+      res.status(201).json({
+        success: true,
+        layout,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
