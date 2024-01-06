b201c718e03e7cf99d2be5657947c619f000f237
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-06 15:00:23 +0100

    creating layout route

---
+++ server/routes/layout.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+import { createLayout } from "../controllers/layout.controller";
+
+const layoutRouter = express.Router();
+
+layoutRouter.post(
+  "/create-layout-admin",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  createLayout
+);
+
+export default layoutRouter;
