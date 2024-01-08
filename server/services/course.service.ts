89ca99befeff0d533bca8f12fd22f9f1919253f8
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-08 10:21:33 +0100

    adding redis for creating a course

--- server/services/course.service.ts
+++ server/services/course.service.ts
@@ -1,11 +1,22 @@
 import { Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import CourseModel from "../models/course.model";
+import { redis } from "../utils/redis";
 
 export const createCourse = CatchAsyncError(
   async (data: any, res: Response) => {
     const course = await CourseModel.create(data);
 
+    try {
+      await redis.set(
+        course._id.toString(),
+        JSON.stringify(course),
+        "EX",
+        604800
+      );
+    } catch (redisError) {
+      console.error("Redis Error:", redisError);
+    }
     res.status(201).json({
       success: true,
       course,
