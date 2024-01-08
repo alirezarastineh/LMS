1a928787c86a2cd0b9d6af2ad9b7c2feac423bed
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-08 12:22:49 +0100

    typo

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
-import { generateLast12MothsData } from "../utils/analytics.generator";
+import { generateLast12MonthsData } from "../utils/analytics.generator";
 import UserModel from "../models/user.model";
 import CourseModel from "../models/course.model";
 import OrderModel from "../models/order.model";
@@ -9,7 +9,7 @@ import OrderModel from "../models/order.model";
 export const getUsersAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const users = await generateLast12MothsData(UserModel);
+      const users = await generateLast12MonthsData(UserModel);
 
       res.status(200).json({
         success: true,
@@ -24,7 +24,7 @@ export const getUsersAnalytics = CatchAsyncError(
 export const getCoursesAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const courses = await generateLast12MothsData(CourseModel);
+      const courses = await generateLast12MonthsData(CourseModel);
 
       res.status(200).json({
         success: true,
@@ -39,7 +39,7 @@ export const getCoursesAnalytics = CatchAsyncError(
 export const getOrdersAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const orders = await generateLast12MothsData(OrderModel);
+      const orders = await generateLast12MonthsData(OrderModel);
 
       res.status(200).json({
         success: true,
