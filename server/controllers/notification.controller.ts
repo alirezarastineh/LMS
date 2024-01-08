7010b11cb1204a870b5f3b7ff8ebaaec31646bf0
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-08 12:54:49 +0100

    change the if statement

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -25,12 +25,15 @@ export const updateNotificationStatus = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const notification = await NotificationModel.findById(req.params.id);
-      if (!notification) {
-        return next(new ErrorHandler("Notification not found", 404));
+
+      if (notification) {
+        notification.status = notification.status
+          ? "read"
+          : notification.status;
+
+        await notification.save();
       } else {
-        notification.status
-          ? (notification.status = "read")
-          : notification?.status;
+        return next(new ErrorHandler("Notification not found", 404));
       }
 
       await notification.save();
@@ -51,6 +54,7 @@ export const updateNotificationStatus = CatchAsyncError(
 
 cron.schedule("0 0 0 * * *", async () => {
   const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
   await NotificationModel.deleteMany({
     status: "read",
     createdAt: { $lt: thirtyDaysAgo },
