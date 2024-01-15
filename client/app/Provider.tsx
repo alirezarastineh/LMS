203c5e6f194464a22e8922b5d74d7b5f31f16e91
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-15 20:32:20 +0100

    first commit

---
+++ client/app/Provider.tsx
@@ -0,0 +1,10 @@
+import { Provider } from "react-redux";
+import { store } from "../redux/features/store";
+
+interface ProvidersProps {
+  children: any;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return <Provider store={store}>{children}</Provider>;
+}
---
+++ client/redux/features/store.ts
@@ -0,0 +1,14 @@
+"use client";
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authSlice from "./auth/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+  },
+  devTools: false,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
