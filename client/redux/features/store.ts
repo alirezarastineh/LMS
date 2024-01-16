969ec2ae3a0eb04edaf6eaea2fe3119e86219a48
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-16 09:59:04 +0100

    adding refresh token and load user endpoints

--- client/redux/features/store.ts
+++ client/redux/features/store.ts
@@ -12,3 +12,14 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+const initializeApp = async () => {
+  await store.dispatch(
+    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
+  );
+  await store.dispatch(
+    apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
+  );
+};
+
+initializeApp();
