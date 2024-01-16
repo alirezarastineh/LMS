ab5bc39e6e664e43ec4b4a250db33e6c1ab6aa9d
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-16 11:50:56 +0100

    adding socialAuth

--- client/app/components/Auth/SignUp.tsx
+++ client/app/components/Auth/SignUp.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -11,6 +12,7 @@ import { FcGoogle } from "react-icons/fc";
 import { styles } from "@/app/styles/styles";
 import { useRegisterMutation } from "@/redux/features/auth/authApi";
 import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type SignupProps = {
   setRoute: (route: string) => void;
@@ -26,6 +28,7 @@ const schema = Yup.object().shape({
 
 const Signup = ({ setRoute }: SignupProps) => {
   const [show, setShow] = useState(false);
+
   const [register, { data, error, isSuccess }] = useRegisterMutation();
 
   useEffect(() => {
@@ -34,11 +37,9 @@ const Signup = ({ setRoute }: SignupProps) => {
       toast.success(message);
       setRoute("Verification");
     }
-    if (error) {
-      if ("data" in error) {
-        const errorData = error as any;
-        toast.error(errorData.data.message);
-      }
+    if (error && "data" in error) {
+      const errorData = error as any;
+      toast.error(errorData.data.message);
     }
   }, [isSuccess, error, data?.message, setRoute]);
 
@@ -60,11 +61,13 @@ const Signup = ({ setRoute }: SignupProps) => {
   return (
     <div className="w-full">
       <h1 className={`${styles.title}`}>Join to E-Learning</h1>
+
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label className={`${styles.label}`} htmlFor="email">
             Enter your Name
           </label>
+
           <input
             type="text"
             name=""
@@ -76,13 +79,16 @@ const Signup = ({ setRoute }: SignupProps) => {
               styles.input
             }`}
           />
+
           {errors.name && touched.name && (
             <span className="text-red-500 pt-2 block">{errors.name}</span>
           )}
         </div>
+
         <label className={`${styles.label}`} htmlFor="email">
           Enter your Email
         </label>
+
         <input
           type="email"
           name=""
@@ -94,15 +100,18 @@ const Signup = ({ setRoute }: SignupProps) => {
             styles.input
           }`}
         />
+
         {errors.email && touched.email && (
           <span className="text-red-500 pt-2 block">{errors.email}</span>
         )}
+
         <div className="w-full mt-5 relative mb-1">
           <label className={`${styles.label}`} htmlFor="email">
             Enter your password
           </label>
+
           <input
-            type={!show ? "password" : "text"}
+            type={show ? "text" : "password"}
             name="password"
             value={values.password}
             onChange={handleChange}
@@ -112,23 +121,26 @@ const Signup = ({ setRoute }: SignupProps) => {
               errors.password && touched.password && "border-red-500"
             } ${styles.input}`}
           />
-          {!show ? (
-            <AiOutlineEyeInvisible
-              className="absolute bottom-3 right-2 z-1 cursor-pointer"
-              size={20}
-              onClick={() => setShow(true)}
-            />
-          ) : (
+
+          {show ? (
             <AiOutlineEye
               className="absolute bottom-3 right-2 z-1 cursor-pointer"
               size={20}
               onClick={() => setShow(false)}
             />
+          ) : (
+            <AiOutlineEyeInvisible
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(true)}
+            />
           )}
         </div>
+
         {errors.password && touched.password && (
           <span className="text-red-500 pt-2 block">{errors.password}</span>
         )}
+
         <div className="w-full mt-5">
           <input type="submit" value="Sign Up" className={`${styles.button}`} />
         </div>
@@ -136,10 +148,21 @@ const Signup = ({ setRoute }: SignupProps) => {
         <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
           Or join with
         </h5>
+
         <div className="flex items-center justify-center my-3">
-          <FcGoogle size={30} className="cursor-pointer mr-2" />
-          <AiFillGithub size={30} className="cursor-pointer ml-2" />
+          <FcGoogle
+            size={30}
+            className="cursor-pointer mr-2"
+            onClick={() => signIn("google")}
+          />
+
+          <AiFillGithub
+            size={30}
+            className="cursor-pointer ml-2"
+            onClick={() => signIn("github")}
+          />
         </div>
+
         <h5 className="text-center pt-4 font-Poppins text-[14px]">
           Already have an account?{" "}
           <span
@@ -150,6 +173,7 @@ const Signup = ({ setRoute }: SignupProps) => {
           </span>
         </h5>
       </form>
+
       <br />
     </div>
   );
