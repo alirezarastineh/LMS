0a92312635b9b73aaee64d4c93778e7aa52722b3
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-21 11:45:03 +0100

    Creating Profile Page

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Profile from "../components/Profile/Profile";
+import Footer from "../components/Footer";
+
+type ProfilePageProps = {};
+
+const Page = ({}: ProfilePageProps) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem] = useState(5);
+  const [route, setRoute] = useState("Login");
+  const { user } = useSelector((state: any) => state.auth);
+
+  return (
+    <div className="min-h-screen">
+      <Protected>
+        <Heading
+          title={`${user?.name}'s Profile`}
+          description="E-Learning is good"
+          keywords="E-Learning is future"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          route={route}
+          setRoute={setRoute}
+        />
+        <Profile user={user} />
+        <Footer />
+      </Protected>
+    </div>
+  );
+};
+
+export default Page;
