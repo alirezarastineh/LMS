f5607c484528e9fa95f5e246184c741728612e88
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-21 11:44:00 +0100

    Creating Protected Admin Hook

---
+++ client/app/hooks/adminProtected.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+import { useSelector } from "react-redux";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export default function AdminProtected({ children }: ProtectedProps) {
+  const { user } = useSelector((state: any) => state.auth);
+
+  if (user) {
+    const isAdmin = user?.role === "admin";
+    return isAdmin ? children : redirect("/");
+  }
+}
