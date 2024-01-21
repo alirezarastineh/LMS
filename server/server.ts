3a0418df0b2e6db5174e6f3d55e2404c41eecfc1
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-21 14:09:27 +0100

    adding Socket to server

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
+import { v2 as cloudinary } from "cloudinary";
+import http from "http";
 import { app } from "./app";
 import connectDB from "./utils/db";
-import { v2 as cloudinary } from "cloudinary";
+import { initSocketServer } from "./socketServer";
 
 require("dotenv").config();
 
-app.listen(process.env.PORT, () => {
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 
   connectDB();
@@ -15,3 +19,5 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
+
+initSocketServer(server);
