afd886639426382523450874f4a805f87fdd26bf
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-22 13:34:26 +0100

    update next.config

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com", "randomuser.me"],
+  },
+  experimental: {
+    reactRoot: true,
+    suppressHydrationWarning: true,
   },
 };
 
