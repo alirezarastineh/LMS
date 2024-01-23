ece54a193e2dfceb83d30bcdf68290c2f71debdf
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-23 10:03:30 +0100

    course access page

---
+++ client/app/course-access/[id]/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+import CourseContent from "@/app/components/Course/CourseContent";
+import Loader from "@/app/components/Loader/Loader";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+
+type PageProps = {
+  params: any;
+};
+
+const Page = ({ params }: PageProps) => {
+  const id = params.id;
+
+  const { isLoading, error, data } = useLoadUserQuery(undefined, {});
+
+  useEffect(() => {
+    if (data) {
+      const isPurchased = data.user.courses.find(
+        (item: any) => item._id === id
+      );
+      if (!isPurchased) {
+        redirect("/");
+      }
+    }
+
+    if (error) {
+      redirect("/");
+    }
+  }, [data, error, id]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <CourseContent id={id} user={data.user} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Page;
