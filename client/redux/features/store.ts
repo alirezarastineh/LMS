e11d9f6ce0589843f3f6984dc4bed3e5a3225662
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-30 13:38:23 +0100

    deleted unused function

--- client/redux/features/store.ts
+++ client/redux/features/store.ts
@@ -15,10 +15,6 @@ export const store = configureStore({
 
 const initializeApp = async () => {
   await store.dispatch(
-    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
-  );
-
-  await store.dispatch(
     apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
   );
 };
