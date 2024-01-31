50f0c0c1c13941a167b11a453d3a1b77e03cb505
alirezarastineh <kouroshrastineh95@gmail.com>
2024-01-31 23:14:36 +0100

    first commit

---
+++ client/app/utils/CoursePlayer.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type CoursePlayerProps = {
+  videoUrl: string;
+  title: string;
+};
+
+const CoursePlayer = ({ videoUrl }: CoursePlayerProps) => {
+  const [videoData, setVideoData] = useState({
+    otp: "",
+    playbackInfo: "",
+  });
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:8000/api/v1/getVdoCipherOTP", {
+        videoId: videoUrl,
+      })
+      .then((res) => {
+        setVideoData(res.data);
+      });
+  }, [videoUrl]);
+
+  return (
+    <div
+      style={{ position: "relative", paddingTop: "56.25%", overflow: "hidden" }}
+    >
+      {videoData.otp && videoData.playbackInfo !== "" && (
+        <iframe
+          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=q6nWRv08dU1EIIRC`}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            border: 0,
+          }}
+          allowFullScreen={true}
+          allow="encrypted-media"
+        />
+      )}
+    </div>
+  );
+};
+
+export default CoursePlayer;
